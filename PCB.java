c681a46f0321bfc02c230b13d949574bef979120
maxjanelle11-cmyk <maxjanelle11@gmail.com>
2026-05-12 16:46:05 -0500

    Place holder pcb class

---
+++ PCB.java
@@ -0,0 +1,13 @@
+public class PCB {
+
+
+    //PLACE HOLD CLASS!!!!!!
+    public int pid;
+    public String name;
+    public String state = "READY"; // Default state 
+
+    public PCB(int pid, String name) {
+        this.pid = pid;
+        this.name = name;
+    }
+}
